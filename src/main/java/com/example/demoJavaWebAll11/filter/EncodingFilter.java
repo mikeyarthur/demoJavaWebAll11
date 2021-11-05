7bd4f6473db71bad23b3c0ea7826b677a0cebb15
miekyarthur <1319624130@qq.com>
2021-11-05 08:27:02 +0800

    <A>1. 新增过滤器，用来处理编码问题，当前过滤器使用utf-8编码；

---
+++ src/main/java/com/example/demoJavaWebAll11/filter/EncodingFilter.java
@@ -0,0 +1,29 @@
+package com.example.demoJavaWebAll11.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+        System.out.println("EncodingFilter.init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("EncodingFilter.doFilter +++++ start");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        request.setCharacterEncoding("utf-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("EncodingFilter.doFilter +++++ end");
+    }
+
+    @Override
+    public void destroy() {
+//        Filter.super.destroy();
+        System.out.println("EncodingFilter.destroy");
+    }
+}
